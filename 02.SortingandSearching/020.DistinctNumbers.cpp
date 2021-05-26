5cbc8aa755a6cc545074a82d344f3418bd9bb983
vero <42807466+SIR-vero@users.noreply.github.com>
2021-05-26 20:50:15 +0530

    INtro

---
+++ 02.SortingandSearching/020.DistinctNumbers.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	set<int> s;
+	int t;
+	for(int i = 0; i < n; i++){
+		cin >> t;
+		s.insert(t);
+	}
+	cout << s.size();
+}
